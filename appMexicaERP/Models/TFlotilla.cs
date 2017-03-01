ac08000ee9f27aea91f26d8d240aaff7bb958217
José Alberto <ovallesoft@gmail.com>
2017-03-01 01:45:57 -0600

    qw
    sss

---
+++ appMexicaERP/Models/TFlotilla.cs
@@ -0,0 +1,49 @@
+﻿using appMexicaERP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace appMexicaERP.Models
+{
+    [Table("flotillas")]
+
+    public class TFlotilla
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int idFlotilla { get; set; }
+        public int idMarca { get; set; }
+        public int modelo { get; set; }
+        public string tipo { get; set; }
+        public string serie { get; set; }
+        public int idCombustible { get; set; }
+        public int litros { get; set; }
+        public int capacidad { get; set; }
+        public string placas { get; set; }
+        public string numEconomico { get; set; }
+        public long kilometraje { get; set; }
+        public string ciaSeguro { get; set; }
+        public DateTime vigenciaIniciaSeguro { get; set; }
+        public DateTime vigenciaFinSeguro { get; set; }
+        public long idEmpleado { get; set; }
+        public int idEmpresa { get; set; }
+        public DateTime ultimoServicio { get; set; }
+        public int tanque { get; set; }
+        public string observaciones { get; set; }
+        public int estatus { get; set; }
+
+        #region Parent
+        #endregion
+
+        #region child
+        [ForeignKey("idFlotilla")]
+        public virtual List<TControlVehicular> childControlVehicular { get; set; }
+        #endregion
+
+    }
+
+
+}
---
+++ appMexicaERP/obj/Release/Package/PackageTmp/Content/miCodigos/js/jsMensajeGlobal.js
@@ -0,0 +1,35 @@
+﻿var timeOutCerrarMensajeGlobal = '';
+
+function mostrarMensajeGlobal(texto, color) {
+    if (timeOutCerrarMensajeGlobal != '') {
+        clearTimeout(timeOutCerrarMensajeGlobal);
+        timeOutCerrarMensajeGlobal = ''
+    }
+
+    $('#mensajeGlobal').css({ 'display': 'block' });
+    $('#mensajeGlobal').css({ 'padding': '7px' });
+
+    $('#mensajeGlobal').css({ 'background-color': color });
+    $('#mensajeGlobal').html('<div>' + texto + '</div>' + '<div onclick="cerrarMensajeGlobal()">Cerrar</div>');
+    $('#mensajeGlobal').stop(true).animate({ 'width': '0px' }, 0);
+    $('#mensajeGlobal').animate({ 'width': '475px' }, 250);
+
+    if ($('#mensajeGlobal').css('background-color') == 'rgb(20, 151, 204)') {
+        timeOutCerrarMensajeGlobal = window.setTimeout(function () {
+            cerrarMensajeGlobal();
+        }, 5250);
+    }
+}
+
+function cerrarMensajeGlobal() {
+    $('#mensajeGlobal').stop(true).animate({ 'width': '0px' }, 250);
+
+    window.setTimeout(function () {
+        $('#mensajeGlobal').css({ 'padding': '0px' });
+        $('#mensajeGlobal').css({ 'display': 'none' });
+    }, 250);
+}
+
+function animacionScrollMovimientoY(idElementoReferencia, topDiferencia) {
+    $("body").animate({ scrollTop: $("#" + idElementoReferencia).offset().top + topDiferencia }, 500);
+}
